354e3c88ce8d1a9054a228aa893b794d796dae50
0xgafu <0xgafu@protonmail.me>
2022-10-20 23:07:02 +0100

    Update linting

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,10 +1,35 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  env: {
+    node: true
+  },
+
+  extends: [
+    "eslint:recommended",
+    'plugin:tailwindcss/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    "next",
+    "turbo",
+    "prettier"
+  ],
+
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'tailwindcss'
+  ],
+
+  parserOptions: {
+    ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser',
+  },
+
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
     "curly": ["error", "multi-line"],
     "vue/multi-word-component-names": ["off"],
-    "no-console": ["off"]
+    "no-console": ["off"],
+    "max-len": ["warn"]
   },
 };
