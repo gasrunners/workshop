06597e3b5f60acb2f1a8d5c395740453699110e4
0xgafu <0xgafu@protonmail.me>
2022-10-21 23:01:58 +0100

    Lint and add Optimisim support

--- packages/evm-fetch/src/index.ts
+++ packages/evm-fetch/src/index.ts
@@ -1,11 +1,7 @@
 import { FunctionFragment, Interface } from '@ethersproject/abi'
 import { JsonRpcProvider } from '@ethersproject/providers'
-import { Contract, ContractInterface } from '@ethersproject/contracts'
+import { Contract } from '@ethersproject/contracts'
 import set from 'lodash.set'
-
-export function add(a: number, b: number): number {
-  return a + b
-}
 
 /**
  * TYPES
@@ -22,19 +18,21 @@ export enum Network {
   MAINNET = '1',
   GOERLI = '5',
   POLYGON = '137',
-  ARBITRUM = '42161'
+  ARBITRUM = '42161',
+  OPTIMISM = '10',
 }
 
 export const MulticallerAddressMap = {
-  '1': '0x5ba1e12693dc8f9c48aad8770482f4739beed696',
-  '5': '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696',
-  '137': '0x275617327c958bD06b5D6b871E7f491D76113dd8',
-  '42161': '0x5ba1e12693dc8f9c48aad8770482f4739beed696'
+  [Network.MAINNET]: '0x5ba1e12693dc8f9c48aad8770482f4739beed696',
+  [Network.GOERLI]: '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696',
+  [Network.POLYGON]: '0x275617327c958bD06b5D6b871E7f491D76113dd8',
+  [Network.ARBITRUM]: '0x80C7DD17B01855a6D2347444a0FCC36136a314de',
+  [Network.OPTIMISM]: '0x2DC0E2aa608532Da689e89e237dF582B783E552C',
 }
 
 export type Args = {
-  network?: Network | null,
-  provider?: JsonRpcProvider | string | null,
+  network?: Network | null
+  provider?: JsonRpcProvider | string | null
   options?: Record<string, any> | null
   requireAll?: boolean | null
 }
@@ -50,7 +48,7 @@ export class EVMFetch {
 
   /**
    * Initialize multicaller instance.
-   * 
+   *
    * @param {Network} args.network - Network to make contract calls on.
    * @param {JsonRpcProvider|string} args.provider - RPC URL or Ethers.js
    * JsonRpcProvider for making contract calls.
@@ -58,7 +56,12 @@ export class EVMFetch {
    * @param {boolean} [args.requireAll=false] - Requires all calls to be
    * successful, otherwise can return partial results.
    */
-  constructor({ network = null, provider = null, options = {}, requireAll = false } = args) {
+  constructor({
+    network = null,
+    provider = null,
+    options = {},
+    requireAll = false,
+  } = args) {
     if (!network) throw new Error('Must pass network argument')
     if (!provider) throw new Error('Must pass provider argument')
 
@@ -70,7 +73,7 @@ export class EVMFetch {
 
   /**
    * Adds a contract call to the calls array.
-   * 
+   *
    * @param {string} callParams.key - Key to use for results object.
    * @param {address} callParams.address - Contract address for call.
    * @param {string|FunctionFragment} callParams.function - Function to call on contract.
@@ -85,11 +88,11 @@ export class EVMFetch {
 
   /**
    * Executes all calls added via addCall.
-   * 
+   *
    * @param {Object} from - An object to inject with results of calls.
    * @returns Object with results at specified key paths.
    */
-  public async fetch<T>(from?: Object): Promise<T> {
+  public async fetch<T = any>(from?: Record<string, unknown>): Promise<T> {
     const obj = from || {}
     const result = await this._fetch()
     result.forEach((r, i) => set(obj, this.paths[i], r))
@@ -131,14 +134,17 @@ export class EVMFetch {
     return new Contract(
       this.address,
       [
-        'function tryAggregate(bool requireSuccess, tuple(address, bytes)[] memory calls) public view returns (tuple(bool, bytes)[] memory returnData)'
+        `function tryAggregate(
+          bool requireSuccess,
+          tuple(address, bytes)[] memory calls
+        ) public view returns (tuple(bool, bytes)[] memory returnData)`,
       ],
       this.provider
     )
   }
 
   private callInterfaces(): Interface[] {
-    return this.calls.map(call => new Interface(call.abi))
+    return this.calls.map((call) => new Interface(call.abi))
   }
 
   private encodedCalls(): Array<string[]> {
@@ -146,7 +152,7 @@ export class EVMFetch {
 
     return this.calls.map((call, i) => [
       call.address.toLowerCase(),
-      interfaces[i].encodeFunctionData(call.function, call.params)
+      interfaces[i].encodeFunctionData(call.function, call.params),
     ])
   }
 
@@ -163,8 +169,8 @@ export class EVMFetch {
       if (typeof provider === 'string') {
         return new JsonRpcProvider(provider)
       } else {
-       return provider
-      }  
+        return provider
+      }
     } catch (error) {
       throw new Error(`EVMFetch unable to establish provider for: ${provider}`)
     }
