689c272a37c2ba072013b7b318e4072d56736b4e
0xgafu <0xgafu@protonmail.me>
2023-01-24 20:26:49 +0000

    Add fontawesome

--- apps/gasrunners-com/src/pages/_app.tsx
+++ apps/gasrunners-com/src/pages/_app.tsx
@@ -3,9 +3,21 @@ import { type AppType } from "next/app";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script
+          src="https://kit.fontawesome.com/1204472a1e.js"
+          crossOrigin="anonymous"
+          async
+        />
+      </Head>
+      <Component {...pageProps} />);
+    </>
+  )
 };
 
 export default api.withTRPC(MyApp);
